8f4451eb8c6b7e39adb60f1d360101a535b95fe1
brad-git03 <bsmanalese.student@ua.edu.ph>
2025-06-20 08:57:37 +0800

    Added Modified

--- session9/index.js
+++ session9/index.js
@@ -28,3 +28,15 @@ const hello = () => {
 }
 
 hello(); // Hello World
+
+// if-else vs shorthand if-else
+
+let age = 18;
+if (age >= 18) {
+    console.log("You are an adult");
+}  else {
+    console.log("You are a minor");
+}
+// Shorthand if-else
+let message = (age >= 18) ? "You are an adult" : "You are a minor";
+console.log(message); // You are an adult
